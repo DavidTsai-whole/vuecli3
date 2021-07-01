2849ff3482e72c891cf1c0c9393cd56aa15c7ba4
david <a22646185@gmail.com>
2021-07-01 11:51:53 +0800

    80

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,14 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#">咕咕<span class="text-primary">雞</span></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
+    <nav class="nav">
+    <div class="container d-flex align-items-center justify-content-between">
+    <a href="#" class="h1">咕咕雞</a>
+    <ul class="d-flex">
+      <li><a href="">商品列表</a></li>
+      <li><a href="">關於我們</a></li>
+      <li><a href=""><i class="bi bi-cart"></i></a></li>
+    </ul>
     </div>
-  </div>
-</nav>
+
+    </nav>
+
 </template>
